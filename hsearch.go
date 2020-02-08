30566b97c968d81f9ee1211fa58f6a678881f735
Hilde N <snhilde@protonmail.com>
2020-02-08 09:43:40 -0500

    LinearInt: check for invalid haystack size

--- hsearch.go
+++ hsearch.go
@@ -8,6 +8,10 @@ import (
 // Find needle's position in haystack.
 // Search progresses linearly.
 func LinearInt(haystack []int, needle int) (int, error) {
+	if len(haystack) < 1 {
+		return -1, errors.New("Invalid haystack size")
+	}
+
 	for i, v := range haystack {
 		if needle == v {
 			return i, nil
