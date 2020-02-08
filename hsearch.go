dfba772ee0be49fa33822cc2d2fefaa08ad0155f
Hilde N <snhilde@protonmail.com>
2020-02-08 16:27:37 -0500

    add BinaryInt() to search sorted arrays in binary fashion

--- hsearch.go
+++ hsearch.go
@@ -20,3 +20,37 @@ func LinearInt(haystack []int, needle int) (int, error) {
 
 	return -1, errors.New("Not found")
 }
+
+// Find needle's position in haystack (which must be sorted).
+// Search progresses binarily.
+func BinaryInt(haystack []int, needle int) (int, error) {
+	// We're going to follow this sequence:
+	// 1. Calculate start and end indices for the block where we want to search.
+	// 2. Determine where the middle of this block is.
+	//     2a. If the middle is the node we're looking for, return that index.
+	//     2b. If it's greater than the value we're looking for, shift the block down so the end is right before it.
+	//     2c. If it's less than the value we're looking for, shift the block up so the beginning is right after it.
+	if len(haystack) < 1 {
+		return -1, errors.New("Invalid haystack size")
+	}
+
+	start := 0
+	end := len(haystack) - 1
+	mid := (start + end) / 2
+	value := haystack[mid]
+	for start < end {
+		if value > needle {
+			end = mid - 1
+		} else if value < needle {
+			start = mid + 1
+		}
+
+		mid = (start + end) / 2
+		value = haystack[mid]
+		if value == needle {
+			return mid, nil
+		}
+	}
+
+	return -1, errors.New("Not found")
+}
