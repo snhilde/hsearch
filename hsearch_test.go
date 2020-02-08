e599a062eda66ed600cf2247f42742de1dd2da04
Hilde N <snhilde@protonmail.com>
2020-02-08 11:24:11 -0500

    test binary search with sorted array

--- hsearch_test.go
+++ hsearch_test.go
@@ -8,8 +8,8 @@ import (
 
 
 // Build two slices of random numbers: one with our target value and one without.
-// Send each slice and target value through the provided hsearc function.
-func testSearch(t *testing.T, searchFunc func([]int, int)(int, error)) {
+// Send each slice and target value through LinearInt().
+func TestLinearInt(t *testing.T) {
 	var listA []int
 	var listB []int
 
@@ -18,7 +18,7 @@ func testSearch(t *testing.T, searchFunc func([]int, int)(int, error)) {
 	random := rand.New(source)
 
 	target := random.Int()
-	length := 1000
+	length := 10000
 
 	// Create our first slice, which will include our target value.
 	for i := 0; i < length; i++ {
@@ -29,8 +29,9 @@ func testSearch(t *testing.T, searchFunc func([]int, int)(int, error)) {
 	listA[index] = target
 
 	// Test the first slice.
-	i, err := searchFunc(listA, target)
+	i, err := LinearInt(listA, target)
 	if err != nil {
+		t.Log("Expected to find target")
 		t.Error(err)
 	} else if i != index {
 		t.Error("Incorrect index")
@@ -48,7 +49,7 @@ func testSearch(t *testing.T, searchFunc func([]int, int)(int, error)) {
 	}
 
 	// Test the second slice.
-	i, err = searchFunc(listB, target)
+	i, err = LinearInt(listB, target)
 	if i != -1 {
 		t.Error("Found unexpected target in list B")
 	} else if err == nil {
@@ -59,10 +60,62 @@ func testSearch(t *testing.T, searchFunc func([]int, int)(int, error)) {
 	}
 }
 
-func TestLinearInt(t *testing.T) {
-	testSearch(t, LinearInt)
-}
-
+// Build two slices of sorted random numbers: one with our target value and one without.
+// Send each slice and target value through BinaryInt().
 func TestBinaryInt(t *testing.T) {
-	testSearch(t, BinaryInt)
+	var listA []int
+	var listB []int
+
+	// Get maximum int.
+	maxInt := int((1 << 31) - 1)
+
+	seed   := time.Now().UnixNano()
+	source := rand.NewSource(seed)
+	random := rand.New(source)
+
+	target := random.Int()
+	length := 10000
+
+	// Create our first slice, which will include our target value.
+	for i := 1; i < length; i++ {
+		section := float64(i) / float64(length)
+		section *= float64(maxInt)
+		v := random.Int31n(int32(section))
+		listA = append(listA, int(v))
+	}
+	index := int(random.Int31n(int32(length)))
+	listA[index] = target
+
+	// Test the first slice.
+	i, err := LinearInt(listA, target)
+	if err != nil {
+		t.Log("Expected to find target")
+		t.Error(err)
+	} else if i != index {
+		t.Error("Incorrect index")
+		t.Log("Expected index =", index)
+		t.Log("Returned index =", i)
+	}
+
+	// Create our second slice, which will not include our target value.
+	for i := 1; i < length; i++ {
+		v := target
+		for v == target {
+			section := float64(i) / float64(length)
+			section *= float64(maxInt)
+			v = int(random.Int31n(int32(section)))
+		}
+		listB = append(listB, v)
+	}
+
+	// Test the second slice.
+	i, err = LinearInt(listB, target)
+	if i != -1 {
+		t.Error("Found unexpected target in list B")
+	} else if err == nil {
+		t.Error("Unexpectedly passed Test 2")
+	} else if err.Error() != "Not found" {
+		t.Error("Received unexpected error:")
+		t.Error(err)
+	}
 }
