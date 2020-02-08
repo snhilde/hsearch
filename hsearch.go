dff68acecec6111820f5ea69da4cf489d81e4168
Hilde N <snhilde@protonmail.com>
2020-02-08 08:46:41 -0500

    add LinearInt search

--- hsearch.go
+++ hsearch.go
@@ -1,10 +1,18 @@
 package hsearch
 
 import (
-	"fmt"
+	"errors"
 )
 
 
-func LinearSearch() {
-	fmt.Println("test worked")
+// Find needle's position in haystack.
+// Search progresses linearly.
+func LinearInt(haystack []int, needle int) (int, error) {
+	for i, v := range haystack {
+		if needle == v {
+			return i, nil
+		}
+	}
+
+	return -1, errors.New("Not found")
 }
