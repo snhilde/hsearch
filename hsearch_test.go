974f7b7b05f78c9ff0a0d81f1ea9c7b297a61e04
Hilde N <snhilde@protonmail.com>
2020-02-08 17:53:27 -0500

    pass in length of slice to test, use make, fix typos

--- hsearch_test.go
+++ hsearch_test.go
@@ -2,45 +2,42 @@ package hsearch
 
 import (
 	"testing"
-	"math/rand"
 	"time"
+	"math/rand"
 	"sort"
 )
 
 
 // Build two slices of random numbers: one with our target value and one without.
-// Send each slice and target value through the provided hsearc function.
+// Send each slice and target value through the provided search function.
 // t          testing object
 // searchFunc callback search function
 // iters      num of iterations to run
+// length     length of slice to sort
 // sorted     whether or not to sort the arrays before searching
-func testSearch(t *testing.T, searchFunc func([]int, int)(int, error), iters int, sorted bool) {
+func testSearch(t *testing.T, searchFunc func([]int, int)(int, error), iters int, length int, sorted bool) {
 	for i := 0; i < iters; i++ {
-		var listA []int
-		var listB []int
-
 		seed   := time.Now().UnixNano()
 		source := rand.NewSource(seed)
 		random := rand.New(source)
 
 		target := random.Int()
-		length := 10000
 
 		// Create our first slice, which will include our target value.
+		list := make([]int, length)
 		for i := 0; i < length; i++ {
-			v := random.Int()
-			listA = append(listA, v)
+			list[i] = random.Int()
 		}
 		index := int(random.Int31n(int32(length)))
-		listA[index] = target
+		list[index] = target
 		if sorted {
 			// Sort the list and find our new target index.
-			sort.Ints(listA)
-			index = sort.SearchInts(listA, target)
+			sort.Ints(list)
+			index = sort.SearchInts(list, target)
 		}
 
 		// Test the first slice.
-		i, err := searchFunc(listA, target)
+		i, err := searchFunc(list, target)
 		if err != nil {
 			t.Log("Expected to find target at index", index)
 			t.Error(err)
@@ -51,19 +48,20 @@ func testSearch(t *testing.T, searchFunc func([]int, int)(int, error), iters int
 		}
 
 		// Create our second slice, which will not include our target value.
+		list = make([]int, length)
 		for i := 0; i < length; i++ {
 			v := target
 			for v == target {
 				v = random.Int()
 			}
-			listB = append(listB, v)
+			list[i] = v
 		}
 		if sorted {
-			sort.Ints(listB)
+			sort.Ints(list)
 		}
 
 		// Test the second slice.
-		i, err = searchFunc(listB, target)
+		i, err = searchFunc(list, target)
 		if i != -1 {
 			t.Error("Found unexpected target in list B")
 		} else if err == nil {
@@ -76,9 +74,9 @@ func testSearch(t *testing.T, searchFunc func([]int, int)(int, error), iters int
 }
 
 func TestLinearInt(t *testing.T) {
-	testSearch(t, LinearInt, 100, false)
+	testSearch(t, LinearInt, 100, 10000, false)
 }
 
 func TestBinaryInt(t *testing.T) {
-	testSearch(t, BinaryInt, 100, true)
+	testSearch(t, BinaryInt, 100, 10000, true)
 }
